04fcba0b8468dbca9a3bcc412fdc5262d9c565d9
saverioperazzo <saverioperazzo@gmail.com>
2023-04-12 10:40:01 -0300

    questão04  getRelatedEmployess finalizada

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const manager = data.employees.some((elementosobjetos) => elementosobjetos.managers.includes(id));
+  return manager;
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const filtro = data.employees.filter((e) => e.managers.includes(managerId))
+    .map((manager) => `${manager.firstName} ${manager.lastName}`);
+
+  return filtro;
 };
 
 module.exports = { isManager, getRelatedEmployees };
+
+// const array = ['saverio', 'thaís', 'josiel'];
+// const filtro = array.filter((e) => e === 'saverio');
+
+// console.log(filtro);
