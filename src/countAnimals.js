f73be89e99f3cd49070ce80a5d1cd5383ef39aff
saverioperazzo <saverioperazzo@gmail.com>
2023-04-13 15:55:22 -0300

    questão 05 contAnimals finalizada

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,34 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    const animais = {};// constante criada para preencher o objeto vazio
+    data.species.forEach((specie) => {
+      animais[specie.name] = specie.residents.length;
+    });
+    return animais;
+  }
+  if (animal.sex) { // acessando o sex pelo objeto animal que parametro
+    return data.species.find((specie) => specie.name === animal.species).residents
+      .filter((resident) => resident.sex === animal.sex).length;
+  }
+  return data.species.find((specie) => specie.name === animal.species).residents.length;
 };
 
+// console.log(countAnimals({ species: 'lions' }));
+// console.log(countAnimals({ species: 'lions', sex: 'female' }));
+
 module.exports = countAnimals;
+
+// if (!animal) {
+//   data.species.reduce((acc, curr) => {
+//     acc[curr.name] = curr.residents.length;
+//     return acc;
+//   }, {});
+// }
+
+// if (!animal.sex) {
+//   return data.species.find((specie) => specie.name === animal.species).residents
+//     .filter(() => );
+// }
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 const isManager = (id) => {
   const manager = data.employees.some((elementosobjetos) => elementosobjetos.managers.includes(id));
   return manager;
-};
+}; // algumas HOFs não criam outra array , eles analizam tudo das outras arrays ou objetos , por isso vc apesar de acessar um array novo , vc chama 'managers'.
 
 const getRelatedEmployees = (managerId) => {
   if (isManager(managerId) === false) {
@@ -12,8 +12,10 @@ const getRelatedEmployees = (managerId) => {
   const filtro = data.employees.filter((e) => e.managers.includes(managerId))
     .map((manager) => `${manager.firstName} ${manager.lastName}`);
 
-  return filtro;
+  return filtro;// retorna o que vc trabalho e não a função que entrou, vc quer retornar o resultado.
 };
+
+// no 'if' vc chama outra funço aqui dentro mas aqui vc tem que chamar o parametro DESSA função , e não da outra.
 
 module.exports = { isManager, getRelatedEmployees };
 
