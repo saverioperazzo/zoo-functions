2f70dec8b2c5bb24120912723485c82d44426221
saverioperazzo <saverioperazzo@gmail.com>
2023-04-18 10:55:30 -0300

    questão 11 12 13

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,8 +1,34 @@
-// const data = require('../data/zoo_data');
+const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-// const getEmployeesCoverage = () => {
-//   if(name )
-//   const test01 = data.employees.find((id01) => id01.id === id);
-// };
+const getSpecies = (emp, prop) => {
+  const searchForSpecies = emp.responsibleFor.map((empNew) =>
+    species.find((emp01) => emp01.id === empNew));
+  return searchForSpecies.map((empNew) => empNew[prop]);
+};
 
-// module.exports = getEmployeesCoverage;
+const retorno = (emp) => ({
+  id: emp.id,
+  fullName: `${emp.firstName} ${emp.lastName}`,
+  species: getSpecies(emp, 'name'),
+  locations: getSpecies(emp, 'location'),
+});
+
+const getEmployeesCoverage = (obj) => {
+  if (obj === undefined) {
+    return employees.map((emp) => retorno(emp));
+  }
+
+  const test01 = employees
+    .find((emp) => emp.firstName === obj.name || emp.lastName === obj.name
+    || emp.id === obj.id);
+
+  if (test01 === undefined) {
+    throw new Error('Informações inválidas');
+  }
+
+  return retorno(test01);
+};
+// getEmployeesCoverage({ name: 'Sharonda' });
+console.log(getEmployeesCoverage({ id: '4b40a139-d4dc-4f09-822d-ec25e819a5ad' }));
+module.exports = getEmployeesCoverage;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,36 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Testes sem argumento devem retornar o objeto', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
+  it('Para os arumentos Monday e 09:00-AM deve retornar a string The zoo is closed', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+  it('Para os arumentos Tuesday e 09:00-AM deve retornar a string The zoo is open', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+  it('Para os arumentos Wednesday e 09:00-AM deve retornar a string The zoo is closed', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+  });
+  it('Para os arumentos Thu e 09:00-AM deve lançar uma exceção com a mensagem: The day must be valid. Example: Monday', () => {
+    expect(() => getOpeningHours('Thu', '09:00-AM')).toThrow('The day must be valid. Example: Monday');
+  });
+  it('Para os arumentos Friday e 09:00-ZM deve retornar a string The abbreviation must be \'AM\' or \'PM\'', () => {
+    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+  });
+  it('Para os arumentos Saturday e C9:00-AM deve retornar a string The abbreviation must be \'AM\' or \'PM\'', () => {
+    expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrow('The hour should represent a number');
+  });
+  it('Para os arumentos Sunday e 09:c0-AM deve retornar a string The abbreviation must be \'AM\' or \'PM\'', () => {
+    expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow('The minutes should represent a number');
+  });
+});
